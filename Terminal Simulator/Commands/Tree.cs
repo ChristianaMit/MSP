74cbaa9a3cd1a11acc7cc9e57823373e67e5d214
ChristianaMit <ni2mc96@gmail.com>
2017-03-22 02:29:21 +0200

    Implemented changes for Tree Command.

--- Terminal Simulator/Commands/Tree.cs
+++ Terminal Simulator/Commands/Tree.cs
@@ -10,6 +10,7 @@ namespace TerminalSimulator
         {
             if( input != null)
                 Console.WriteLine("Command doesn't accept parameters");
+            
             else
                 buildTree(file);
         }
@@ -17,26 +18,39 @@ namespace TerminalSimulator
 
         public void buildTree (FileSystemReader file){
             
-            string tree = file.AllDirectories[0].name;
-            string brench = "\n" + "|" + "\n" + " " + "---" + " ";
-
-            Directory tempDirectory = new Directory();
-
-            for(int i=0; i<(file.numberOfDirectories); i++){
-                tempDirectory = file.AllDirectories[i];
-                tree = findChildren (tree, brench, tempDirectory);
-                brench = "  "+brench;
-            }
-
+            string tree = "\n" + file.curentDirectory.name;
+            string brench = "\n" + "    |" + "\n" + " " + "    ---" + " ";
+            
+            tree = findChildren (tree, brench, file);
             Console.WriteLine(tree);
         }
 
-        public string findChildren (string tree,string brench, Directory curentDirectory){
+
+        public string findChildren (string tree,string brench, FileSystemReader file){
             
-            if(curentDirectory.children != null){
-                foreach(var child in curentDirectory.children){
-                    tree = tree+ brench + child.name; 
+            if(file.curentDirectory.children != null){
+                int cDNrChildrn=0;
+                while(cDNrChildrn <= file.curentDirectory.children.Count){
+                    foreach(var child in file.curentDirectory.children){
+                        cDNrChildrn++;
+                        tree = tree+ brench + child.name;
+                        Console.WriteLine(tree);
+                        DirectorySetter changeDir = new DirectorySetter();
+                        if(child is TerminalSimulator.Directory){
+                            changeDir.SetCurentDirectory(child.name, file);
+                            findChildren(tree, brench, file);
+                        }
+                        else
+                            tree = tree + brench + child.name; 
+                    }
                 }
+                DirectorySetter changeDirBackwords = new DirectorySetter();
+                changeDirBackwords.SetCurentDirectory(file.curentDirectory.parent.name, file);
+            }
+
+            if(file.curentDirectory.children == null){
+                DirectorySetter changeDir = new DirectorySetter();
+                changeDir.SetCurentDirectory(file.curentDirectory.parent.name, file);
             }
 
             return tree;
@@ -44,6 +58,20 @@ namespace TerminalSimulator
     }
 }
 
+
+/*
+ while(name.Equals(file.curentDirectory.name) == false){
+                foreach(var child in file.curentDirectory.children){
+                    for(int i=0; i<file.numberOfDirectories; i++)
+                        if(file.AllDirectories[i].name.Equals(child.name))
+                            file.curentDirectory=file.AllDirectories[i];
+                    if(child.name == name)
+                        break;
+                    else
+                        SetCurentDirectory(name, file);
+                }   
+
+ */
 
 /*
 public void Execute(string input, FileSystemReader file)
--- Terminal Simulator/Program.cs
+++ Terminal Simulator/Program.cs
@@ -11,20 +11,25 @@ namespace TerminalSimulator
             Console.Write("Password:");
             string pass= Console.ReadLine();
             Console.WriteLine("\n   Welcome back {0}!\n", name);*/
-
             
-
             string name= "Kitty";
             FileSystemReader allDirectories = new FileSystemReader();
 
             Reader read = new Reader();
-            read.InputReader(name, allDirectories);                     //Reads from a file sysem model
+            read.InputReader(name, allDirectories);                  //Reads from a file sysem model
             
-            OutputWriter printerOnScreen = new OutputWriter();
-            printerOnScreen.WriteToScreen(allDirectories);
+            Console.WriteLine("Do you want to see the file system before exiting? (y/n)");
+            string answer = Console.ReadLine();
+            if(answer == "y"){
+                OutputWriter printerOnScreen = new OutputWriter();
+                printerOnScreen.WriteToScreen(allDirectories);
+            }
+            
         }
     }
 }
+
+
 
 //To implement: 
             //1. Class Users with list of accepted users and passwords
